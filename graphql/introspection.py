21249c24c5d6b61005876055369b59719268e874
pjlanger1 <113559711+pjlanger1@users.noreply.github.com>
2025-02-21 13:13:01 -0500

    Create introspection.py

---
+++ graphql/introspection.py
@@ -0,0 +1,57 @@
+import requests
+import json
+
+url = "https://gateway.production.kiva.org/graphql"
+
+introspection_query = """
+{
+  __schema {
+    types {
+      name
+      kind
+      possibleTypes {
+        name
+      }
+      fields {
+        name
+        args {
+          name
+          type {
+            name
+          }
+        }
+        type {
+          name
+        }
+      }
+      inputFields {
+        name
+        type {
+          name
+        }
+      }
+      interfaces {
+        name
+      }
+      enumValues {
+        name
+      }
+      ofType {
+        name
+      }
+    }
+  }
+}
+"""
+
+response = requests.post(
+    url,
+    headers={"Content-Type": "application/json"},
+    data=json.dumps({"query": introspection_query})
+)
+
+
+data = response.json()
+
+#print out the human-readable schema 
+[t for t in data.get('data').get('__schema').get('types') if t.get('name')]
